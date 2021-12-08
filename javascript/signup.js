50db0054f913a70e0c29cc9cb0dfe579a0321f21
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-12-09 02:15:00 +0530

    AJAX for getting a response from the php file

--- javascript/signup.js
+++ javascript/signup.js
@@ -9,6 +9,13 @@ continueBtn.onclick = () => {
     // AJAX
     let xhr = new XMLHttpRequest();
     xhr.open('POST', 'php/signup.php', true);
-    xhr.onload = () => {}
+    xhr.onload = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                let data = xhr.response;
+                console.log(data);
+            }
+        }
+    }
     xhr.send();
 }
