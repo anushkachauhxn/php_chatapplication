9d898ad5f69e0e8893fc43bc9b351232da7fbc7a
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-12-09 02:53:00 +0530

    PHP Code for Signup Form: 2.1 Email validity

--- php/signup.php
+++ php/signup.php
@@ -8,7 +8,13 @@
 
     // #1 Check if all inputs are filled
     if (!empty($fname) && !empty($lname) && !empty($email) && !empty($password)) {
-        
+        // #2.1 Email validity
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+        }
+        else {
+            echo "$email - This is not a valid email";
+        } // else #2.1
     }
     else {
         echo "All input fields are required";
