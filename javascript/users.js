d3a6e96490cfc23f0eb62be9dd3a70b5fc1a89c1
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-12-10 18:15:00 +0530

    Making Users Page Dynamic: 3 Search Bar Functionality

--- javascript/users.js
+++ javascript/users.js
@@ -9,6 +9,23 @@ searchBtn.onclick = () => {
     searchBar.focus()
 }
 
+searchBar.onkeyup = () => {
+    let searchTerm = searchBar.value;
+    // AJAX
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'php/search.php', true);
+    xhr.onload = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                let data = xhr.response;
+                usersList.innerHTML = data;
+            }
+        }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("searchTerm=" + searchTerm);
+}
+
 /* Making users-list dynamic using AJAX */
 setInterval(() => {
     // AJAX
---
+++ php/search.php
@@ -0,0 +1,26 @@
+<?php
+    include_once "config.php";
+    $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
+    $output = "";
+
+    $sql = mysqli_query($conn, "SELECT * FROM users WHERE fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%'");
+    if (mysqli_num_rows($sql) > 0) {
+        while ($row = mysqli_fetch_assoc($sql)) {
+            $output .= '<a href="#">
+                            <div class="content">
+                            <img src="uploaded_images/' . $row['img'] . '" alt="" />
+                            <div class="details">
+                                <span>'. $row['fname'] . ' ' . $row['lname'] .'</span>
+                                <p>This is a test</p>
+                            </div>
+                            </div>
+                            <div class="status-dot"><i class="fas fa-circle"></i></div>
+                        </a>';
+        }
+    }
+    else {
+        $output .= "No user found related to your search";
+    }
+
+    echo $output;
+?>
