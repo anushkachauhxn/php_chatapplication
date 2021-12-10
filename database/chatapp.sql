9e9a279be4a9abd6016e5cc5bcc6e0e9ab0828b8
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-12-10 18:29:15 +0530

    Database changes for chat messages

--- database/chatapp.sql
+++ database/chatapp.sql
@@ -5,7 +5,7 @@ SET time_zone = "+05:30";
 -- Table structure for table `user`
 --
 CREATE TABLE `users` (
-  `user_id`     int(200) NOT NULL,
+  `user_id`     int(200) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `unique_id`   int(200) NOT NULL,
   `fname`       varchar(255) NOT NULL,
   `lname`       varchar(255) NOT NULL,
@@ -13,5 +13,15 @@ CREATE TABLE `users` (
   `password`    varchar(255) NOT NULL,
   `img`         varchar(400) NOT NULL,
   `status`      varchar(255) NOT NULL
+);
+
+--
+-- Table structure for table `messages`
+--
+CREATE TABLE `messages` (
+  `msg_id`            int(255) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `incoming_msg_id`   int(255) NOT NULL,
+  `outgoing_msg_id`   int(255) NOT NULL,
+  `msg`               varchar(1000) NOT NULL
 );
 
