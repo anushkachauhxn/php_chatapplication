605fa07c5d24c796f2d9a88aa1b8665e2385640d
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-12-11 10:42:00 +0530

    Logout Functionality Change status of user to 'Offline Now' in database

--- users.php
+++ users.php
@@ -36,7 +36,7 @@
               <p><?php echo $row['status'] ?></p>
             </div>
           </div>
-          <a href="#" class="logout">Logout</a>
+          <a href="php/logout.php?logout_id=<?php echo $row['unique_id'] ?>" class="logout">Logout</a>
         </header>
 
         <div class="search">
